c53af06cfb311c060626a6ca2960bd2c226e0bd2
dusanov <sadriddindusanov@gmail.com>
2026-04-16 19:01:44 +0500

    Insert function

--- src/main/java/dasturlash/uz/Main.java
+++ src/main/java/dasturlash/uz/Main.java
@@ -1,6 +1,11 @@
 package dasturlash.uz;
 
+import dasturlash.uz.entity.Hospital;
+import dasturlash.uz.entity.Transport;
+import dasturlash.uz.enums.TransportType;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
@@ -8,12 +13,26 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class Main {
     static void main() {
+        insertCar();
+    }
+
+    public static void insertCar() {
         StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate-config.xml").build();
         Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();
 
         SessionFactory factory = meta.getSessionFactoryBuilder().build();
+        Session session = factory.openSession();
+        Transaction t = session.beginTransaction();
+
+        Transport transport = new Transport();
+        transport.setCode("1234");
+        transport.setType(TransportType.AMBULANCE);
+        transport.setHospitalId(1);
+        session.save(transport);
 
 
         factory.close();
+        session.close();
     }
+
 }
